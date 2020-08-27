701014f4a13a8f34709efc99a71240ae5205477e
aindriu80 <aindriu80@gmail.com>
2020-08-27 21:25:57 +0100

    Setting up the bot - hello msg

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,10 @@
 require("dotenv").config();
 
-console.log(process.env.DISCORDJS_BOT_TOKEN);
+const { Client } = require("discord.js");
+const client = new Client();
+
+client.on("ready", () => {
+  console.log(`${client.user.tag} has logged in`);
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
